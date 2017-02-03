1c738163dc713a8fe8f4939b718988cd5fc58a65
Alex Corstorphine <alex@dome9.com>
2017-02-03 08:44:57 -0800

    Added filtering to main file

--- SNStoSlack.js
+++ SNStoSlack.js
@@ -1,6 +1,19 @@
-/* 
+/*
 Lambda function for pushing events from SNS to Slack
 Modified from the Cloudwatch to Slack template and adopted for Dome9  1.19.2017
+
+Variables needed:
+hookUrl - slakc webhook url
+slackChannel - individual channel to post to
+
+Optional variable:
+messageTypeBlacklist - pipe delimited list of message types to NOT post to slack. This is to help reduce noise
+
+The top 3 most common events you might want to filter are:
+InstanceStateChanged
+InstanceCreatedEvent
+InstanceTagsChangeDetectedEvent
+
 */
 
 'use strict';
@@ -11,9 +24,12 @@ const https = require('https');
 
 // The Slack webhook URL
 const hookUrl = process.env.hookUrl;
+
 // The Slack channel to send a message to stored in the slackChannel environment variable
 const slackChannel = process.env.slackChannel;
 
+// The event types we DON'T want to print (split by pipe)
+var messageTypeBlacklist = process.env.messageTypeBlacklist ? process.env.messageTypeBlacklist.split("|"): [];
 
 function postMessage(message, callback) {
     const body = JSON.stringify(message);
@@ -45,15 +61,18 @@ function postMessage(message, callback) {
 }
 
 function processEvent(event, callback) {
-   var message = event.Records[0].Sns.Message;
-   message = message.split(",").join("\n"); // you can comment out this line to send the message as a blob
+    var message = event.Records[0].Sns.Message;
+    message = message.split(",").join("\n"); // you can comment out this line to send the message as a blob
+    console.log('From SNS:', message);
 
-   console.log('From SNS:', message);
-    
     const slackMessage = {
         channel: slackChannel,
         text: message
     };
+
+    if ((messageTypeBlacklist.filter((n) => message.indexOf(n) >= 0)).length > 0) {
+        return; // if the message matches our blacklist - no need to do anything
+    }
 
     postMessage(slackMessage, (response) => {
         if (response.statusCode < 400) {
@@ -69,6 +88,7 @@ function processEvent(event, callback) {
     });
 }
 
+
 exports.handler = (event, context, callback) => {
-        processEvent(event, callback);
+    processEvent(event, callback);
 };
