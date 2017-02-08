4d77e9cf3c4a7204c5fab09bce4ad9e2e811787b
Alex Corstorphine <alex@dome9.com>
2017-02-08 11:39:14 -0800

    updated comments

--- SNStoSlack.js
+++ SNStoSlack.js
@@ -2,11 +2,14 @@
 /*
 Lambda function for pushing events from SNS to Slack
 Modified from the Cloudwatch to Slack template and adopted for Dome9  1.19.2017
+
 Variables needed:
 hookUrl - slack webhook url
 slackChannel - individual channel to post to
+
 Optional variable:
 messageTypeBlacklist - pipe delimited list of message types to NOT post to slack. This is to help reduce noise
+
 The top 3 most common events you might want to filter are:
 InstanceStateChanged
 InstanceCreatedEvent
